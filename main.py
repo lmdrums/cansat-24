58030b66123bd0fb1a33e134956d53f51f0fa5bb
lmdrums <lewis@stork.org.uk>
2024-09-16 20:05:01 +0100

    Changed logo, got rid of unnecessary files etc.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from apollosat.gui import main as gui
+import apollosat.helpers as h
+import apollosat.constants as c
 
 def main():
+    h.gradient_text(c.ASCII_LOGO)
     gui()
 
 if __name__ == "__main__":
