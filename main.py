eb7f33846d74bc3d779141d3d5465fc3f3955519
lmdrums <lewis@stork.org.uk>
2025-01-28 08:04:21 +0000

    Updated code

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import apollosat.helpers as h
 import apollosat.constants as c
 
 def main():
-    #h.gradient_text(c.ASCII_LOGO)
-    #h.permanent_file(c.PERMANENT_DATA)
-    h.wipe_file(c.MAIN_DATA)
-    gui()
+    """Run ApolloSat"""
+
+    #h.gradient_text(c.ASCII_LOGO) # Fancy logo on startup
+    #h.permanent_file(c.PERMANENT_DATA) # Write data to a permanent file (so data is not deleted on next run)
+    h.wipe_file(c.MAIN_DATA) # Clean data file on each run to keep concise
+    gui() # Open GUI
 
 if __name__ == "__main__":
     main()
